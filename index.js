9b46429b8cd82816378d513bac9e23f244d020c9
dannyflatiron <dannyflatiron@gmail.com>
2020-05-23 23:39:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+// Array functions
+// append
+//   1) appends pet "Odie" to the end of the pets array
+// prepend
+//   2) prepends a pet to the beginning of the pets array
+// removeLast()
+//   3) removes the last pet from the pets array
+// removeFirst()
+//   4) removes the First pet from the pets array
+
+
+
+const append = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
+
+const prepend = ["Milo", "Otis", "Garfield"]
+
+prepend.unshift("Odie")
+
+const removeLast = ["Milo", "Otis", "Garfield"]
+
+removeLast.pop()
+
+const removeFirst = ["Milo", "Otis", "Garfield"]
+
+removeFirst.shift()
+
+
+
+
